043248ee67b080a0e9dc80d883c221b9f35bf6b2
IamA1536 <18974735866@163.com>
2018-10-13 17:20:09 +0800

    Finish Example11

---
+++ example11/FeedRabbit.py
@@ -0,0 +1,16 @@
+a = 2
+b = 0
+n = int(input("Please input the number of month: "))
+sum = 0;
+count = 0;
+m = 0
+print("初始，兔子数：2")
+for i in range(1, n+1):
+    m += 1
+    count += 1
+    if count == 3:
+        a += b
+        sum += a // 2
+        b = a // 2
+        count = 0
+    print("第{0}月，兔子数:{1}".format(m, a + b))
