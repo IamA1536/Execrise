ff8bce342a90a157cd638a4370ad615541552a60
IamA1536 <18974735866@163.com>
2018-11-03 14:34:44 +0800

    Finish example15

---
+++ com/a/example15/ScoreClassification.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    i = int(input("Please input the score: "))
+    if i >= 90:
+        print("A")
+    elif i < 90 and i >= 60:
+        print("B")
+    else:
+        print("C")
