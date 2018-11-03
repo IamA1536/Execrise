39e42bd32b15a0c5424e060c669354982912520a
IamA1536 <18974735866@163.com>
2018-11-03 14:34:52 +0800

    Finish example16

---
+++ com/a/example16/PrintDate.py
@@ -0,0 +1,6 @@
+import datetime
+
+if __name__ == "__main__":
+        print(datetime.date.today())
+        print(datetime.date.today().strftime('%d/%m %Y'))
+        print(datetime.datetime.now())
