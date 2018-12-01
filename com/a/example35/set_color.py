f2153817b2c92547e5294d525fefcb8f4f16bd96
IamA1536 <18974735866@163.com>
2018-12-01 17:41:01 +0800

    Finish example35

---
+++ com/a/example35/set_color.py
@@ -0,0 +1,11 @@
+class wcolors:
+    Header = '\033[1;32;41m'
+    End = '\033[0m'
+
+
+if __name__ == "__main__":
+    print('\033[1;32;41m' + "提示信息" + '\033[0m')
+    # print(wcolors.Header + "Test")
+    # print("提示信息")
+# if __name__ == "__main__":
+#     print("-------------提示信息----------")
