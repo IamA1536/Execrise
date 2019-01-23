da6ae23316a78eee100322b4d1c7571091ed005c
IamA <1694522669@QQ.COM>
2019-01-23 18:19:39 +0800

    Finish example51 52 53 54 55 56 57 58

---
+++ com/a/example53/xor.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    a = 0o77
+    print(a ^ 3)
+    a = a ^ 3
+    print(a ^ 7)
---
+++ com/a/example57/line.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+if __name__ == '__main__':
+    canvas = Canvas(width=600, height=400, bg='white')
+    canvas.pack(expand=YES, fill=BOTH)
+    x0 = 263
+    y0 = 263
+    y1 = 275
+    x1 = 275
+    for i in range(19):
+        canvas.create_line(x0, y0, x0, y1, width=1, fill='red')
+        x0 = x0 - 5
+        y0 = y0 - 5
+        x1 = x1 + 5
+        y1 = y1 + 5
+
+    x0 = 263
+    y1 = 275
+    y0 = 263
+    for i in range(21):
+        canvas.create_line(x0, y0, x0, y1, fill='red')
+        x0 += 5
+        y0 += 5
+        y1 += 5
+
+    mainloop()
