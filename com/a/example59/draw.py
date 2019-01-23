412a5a11eb27eca73caf8d2bbe68c8cdd8124619
IamA <1694522669@QQ.COM>
2019-01-23 18:29:53 +0800

    Finish example59

---
+++ com/a/example59/draw.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : draw.py
+# @Author: A
+# @Date  : 2019/1/23
+# @Contact : qq1694522669@gmail.com
+
+#自己随便画
+#Draw freely by yourself
+#自分で勝手に描く
